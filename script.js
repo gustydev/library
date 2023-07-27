ed673c191a56425509c00d6ae6ae1518bdea32dd
gusty <gustavodiorlando@gmail.com>
2023-07-27 12:54:00 -0300

    Create basic files and library code

---
+++ script.js
@@ -0,0 +1,13 @@
+let library = [];
+
+function Book(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBook(author, title, pages, read) {
+    let newBook = new Book(author, title, pages, read);
+    library.push(newBook);
+}
