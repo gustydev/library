33039e345b1fe937be3493d71e96302129e5f1fd
gusty <gustavodiorlando@gmail.com>
2023-07-27 13:45:17 -0300

    Create code for displaying books on page + basic styling

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let library = [];
+let library = [{author: 'Gusty', title: "Gusty's Book for Testing the Thing", pages: 42, read: false}];
+
+const bookDisplay = document.querySelector('.main-container');
 
 function Book(author, title, pages, read) {
     this.author = author;
@@ -10,4 +12,19 @@ function Book(author, title, pages, read) {
 function addBook(author, title, pages, read) {
     let newBook = new Book(author, title, pages, read);
     library.push(newBook);
+    updateLibrary();
 }
+
+function updateLibrary() {
+    library.forEach(book => {
+      let newBook = document.createElement('div.book');
+        for (const info in book) {
+            let bookInfo = document.createElement(`div.${info}`);
+            bookInfo.textContent = `${info}: ${book[info]}`
+            newBook.appendChild(bookInfo);
+        }
+        bookDisplay.appendChild(newBook);
+})
+};
+
+updateLibrary();
